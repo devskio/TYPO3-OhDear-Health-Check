831070c4910cbbfd610b37cc8925255a22962f30
denys.yefimenko <denys.yefimenko@devsk.io>
2024-04-16 14:42:04 +0200

    [TASK] Localisation and error handling

--- Classes/Widgets/HealthcheckWidget.php
+++ Classes/Widgets/HealthcheckWidget.php
@@ -9,6 +9,7 @@ use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
 class HealthcheckWidget implements WidgetInterface
@@ -27,6 +28,11 @@ class HealthcheckWidget implements WidgetInterface
      * @var RequestFactory
      */
     private $requestFactory;
+
+    /**
+     * @var string
+     */
+    private $summaryStatus;
 
 
     /**
@@ -66,7 +72,24 @@ class HealthcheckWidget implements WidgetInterface
     public function renderWidgetContent(): string
     {
         if ($GLOBALS['BE_USER']->isAdmin()) {
-            $applicationHealthChecks = $this->ohDear->applicationHealthChecks($this->siteId);
+            try {
+                $applicationHealthChecks = $this->ohDear->applicationHealthChecks($this->siteId);
+                foreach ($applicationHealthChecks as $check) {
+                    $this->controlSummaryStatus($check->status);
+                }
+            } catch (\Exception $e) {
+                $this->view->assignMultiple([
+                    'error' => [
+                        'label' => LocalizationUtility::translate(
+                            'LLL:EXT:typo3_ohdear_health_check/Resources/Private/Languages/locallang_backend.xlf:connection.error'
+                        ),
+                        'instructions' => LocalizationUtility::translate(
+                            'LLL:EXT:typo3_ohdear_health_check/Resources/Private/Languages/locallang_backend.xlf:connection.error.instructions'
+                        ),
+                        'message' => $e->getMessage()
+                    ]
+                ]);
+            }
         }
 
         if (!empty($this->siteId) && isset($this->ohDear)) {
@@ -74,6 +97,7 @@ class HealthcheckWidget implements WidgetInterface
         }
 
         foreach ($site->checks as $check) {
+            $this->controlSummaryStatus($check->attributes['latest_run_result']);
             $check->type = $this->formatCheckType($check->type);
         }
 
@@ -81,7 +105,8 @@ class HealthcheckWidget implements WidgetInterface
             'configuration' => $this->configuration,
             'applicationHealthResults' => $applicationHealthChecks ?? null,
             'basicChecks' => $site->checks ?? null,
-            'siteId' => $this->siteId
+            'siteId' => $this->siteId,
+            'summaryStatus' => $this->summaryStatus
         ]);
         return $this->view->render();
     }
@@ -95,5 +120,32 @@ class HealthcheckWidget implements WidgetInterface
     private function formatCheckType(string $checkType): string
     {
         return str_replace('_', '-', $checkType);
+    }
+
+    /**
+     * Controlling the summary status
+     *
+     * @param string $checkSummary
+     * @return void
+     */
+    private function controlSummaryStatus(string $checkSummary): void
+    {
+        if ($this->summaryStatus === 'danger') {
+            return;
+        }
+        if ($this->summaryStatus === 'warning' && $checkSummary !== 'danger') {
+            return;
+        }
+
+        switch ($checkSummary) {
+            case 'warning':
+                $this->summaryStatus = 'warning';
+                break;
+            case 'failed':
+                $this->summaryStatus = 'danger';
+                break;
+            default:
+                $this->summaryStatus = 'success';
+        }
     }
 }
