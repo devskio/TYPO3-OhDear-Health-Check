499904e3ac26f64dd8cb057ec225a81600f27b8b
Jan Vodila <jan@devsk.io>
2024-06-06 23:17:02 +0200

    [TASK] don't cache output due to secret check

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+$EM_CONF['typo3_ohdear_health_check'] = [
+    'title' => 'TYPO3 Oh Dear Health Check',
+    'description' => 'TYPO3 Oh Dear Health Check',
+    'category' => 'be',
+    'author' => 'DEVSK.io',
+    'author_email' => 'development@devsk.io',
+    'state' => 'stable',
+    'version' => '11.4.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.0.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
